e118f40431c25b42afb9df6666257902cb6cd349
Melvin Isican <mj.isican.ceo@gmail.com>
2025-12-08 18:37:40 +0800

    Updated Website Portfolio of Melvin

---
+++ src/constants/index.js
@@ -0,0 +1,169 @@
+import {
+  c,
+  python,
+  java,
+  yondu,
+  reapit,
+  vacation,
+  ts,
+  cpp,
+  javascript,
+  turborepo,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  edunet,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  threejs,
+  mhft,
+  sketcher,
+  mathwork,
+  CompileVortex,
+  eduskill,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "TypeScript", icon: ts },
+  { title: "React", icon: reactjs },
+  { title: "Python", icon: python },
+  { title: "Java", icon: java },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: html },
+  { name: "CSS 3", icon: css },
+  { name: "JavaScript", icon: javascript },
+  { name: "Rect JS", icon: reactjs },
+  { name: "Tailwind CSS", icon: tailwind },
+  { name: "Node JS", icon: nodejs },
+  { name: "Three JS", icon: threejs },
+  { name: "git", icon: git },
+  { name: "turborepo", icon: turborepo },
+];
+
+export const experiences = [
+  {
+    title: "Sr. Software Engineer (Multiple Projects and R&D)",
+    company_name: "Yondu Inc",
+    icon: yondu,
+    iconBg: "#ffffff",
+    date: "June 2024 - Present",
+    points: [
+      "Handled Multiple Projects involving the development of new projects like Advanced Queuing System, Delivery Tracking Mobile App, and a Loyalty App",
+      "Collaborated with some developers to work on some of the company's R&D projects where we develop proof of concepts for new technologies and frameworks",
+    ],
+  },
+  {
+    title: "Sr. Software Engineer (Production Lead L3 Operations)",
+    company_name: "Yondu Inc",
+    icon: yondu,
+    iconBg: "#ffffff",
+    date: "April 2023 to June 2024",
+    points: [
+      "Lead the L3 Operations team of the Mobile Banking Application we completed",
+      "Ensure the seamless deployment and maintenance of application in production environments, minimizing downtime and optimizing performance.",
+      "Collaborated closely with development and QA teams to implement robust monitoring and alerting systems, enabling proactive identification and resolution of potential issues before they impact end-users.",
+      "Developed and maintained comprehensive documentation for deployment processes, incident response protocols, and system configurations, ensuring knowledge sharing and continuity within the team.",
+      "Had an opportunity to lead a team of L2 operation specialists and L3 developers. Mentored non-dev L2 specialists to become developers.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Yondu Inc",
+    icon: yondu,
+    iconBg: "#ffffff",
+    date: "April 2023 to June 2024",
+    points: [
+      "Worked on a Mobile Banking Application for a major Philippine bank using React Native.",
+      "Collaborated with cross-functional teams to define, design, and ship new features while ensuring the performance, quality, and responsiveness of the application.",
+      "Implemented robust security measures to safeguard sensitive user data and ensure compliance with industry standards and regulations.",
+      "Participated in code reviews, testing, and debugging to ensure the reliability and performance of the mobile banking application.",
+      "Involved in the completion of the project which is now live and being used by millions of users.",
+    ],
+  },
+  {
+    title: "Entrepreneur / Freelancer",
+    company_name: "Career Break",
+    icon: vacation,
+    iconBg: "#fff",
+    date: "Sept 2019 to Dec 2020",
+    points: [
+      "Took a career break to focus on personal growth and exploration.",
+      "Traveled to different places in the Philippines to experience diverse cultures and broaden my perspective.",
+      "Explored my entrepreneurial side by starting an Airbnb business in my hometown.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Reapit Sales (formerly Agentbox)",
+    icon: reapit,
+    iconBg: "#fff",
+    date: "June 2017 to Sept 2019",
+    points: [
+      "One of the pioneer devs for the Real Estate CRM system, focusing building their mobile platform using React Native in the first year.",
+      "Collaborated with cross-functional teams to deliver high-quality software solutions that met client requirements and enhanced user experience.",
+      "Worked on various modules including Property Listings, Client Management, and Sales Pipeline.",
+      "Contributed on their next phase improving their web platform using ReactJS.",
+      "Participated in code reviews, testing, and debugging to ensure the reliability and performance of the CRM system.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "GWILL Web Services",
+    icon: mathwork,
+    iconBg: "#161329",
+    date: "April 2016 to May 2017",
+    points: [
+      "Involvement on a bigger project that dealt with developing web apps with ReactJS and NodeJS for singaporean clients.",
+      "Was tasked to develop a web-based POS system for one of the clients using the MERN stack.",
+      "Worked closely with my co-developers to deliver high-quality software that met client requirements.",
+      "Mentored 3 junior developers, providing guidance. Also provided knowledge transfer of the projects that I worked on",
+    ],
+  },
+  {
+    title: "Jr. Full Stack Developer Intern",
+    company_name: "GWILL Web Services",
+    icon: mathwork,
+    iconBg: "#161329",
+    date: "April 2015 to April 2016",
+    points: [
+      "Explored emerging javaScript frameworks and libraries to enhance front-end development processes.",
+      "Worked on web projects that involved dealing with singaporean clients.",
+      "Work also involved creating UI mockups and prototypes for client presentations.",
+    ],
+  },
+];
+
+export const projects = [
+  // {
+  //   name: "App Name Here",
+  //   description:
+  //     "Web-based platform add more description",
+  //   tags: [
+  //     { name: "Javascript", color: "blue-text-gradient" },
+  //     { name: "HTML", color: "green-text-gradient" },
+  //   ],
+  //   image: import_path_here,
+  //   source_code_link: "https://github.com/ceomji/app-name",
+  // },
+];
