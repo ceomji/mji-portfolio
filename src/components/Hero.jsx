5ec2d4cb8707f018ce3f28cf8cd4e8dda335af62
Melvin Isican <mj.isican.ceo@gmail.com>
2025-12-09 08:20:53 +0800

    chore: remove bg-pink that was used to debug the container

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       {/* Background & Intro Container */}
-      <div className={`absolute bg-pink-500 bg-opacity-50 inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10 pointer-events-none`}>
+      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10 pointer-events-none`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
